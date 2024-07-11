90a9a04bfcc905178848d4415693f56ced2cb2a3
Irvin Qi <irvin@MacBook-Pro-2.local>
2024-07-11 10:24:05 -0700

    added voice function to frontend

--- frontend/src/ChatPage.js
+++ frontend/src/ChatPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import smile from './smile.jpg'; 
+import Vapi from "@vapi-ai/web";
 import './index.css'
+
+const vapi = new Vapi("05a15f00-9fd2-4923-a02b-39cd5723c558");
 
 const ChatPage = () => {
 return(
@@ -16,7 +19,7 @@ return(
             <h5> Think of me as you personal waiter!</h5>
             <div className="search-bar-container">
                 <input type="text" placeholder="Type your message..." className="search-input"/>
-                <button className="pressme"> Press to Talk!</button>
+                <button className="pressme" onClick={handleButtonClick}> Press to Talk!</button>
 
             </div>
         </div>
@@ -24,4 +27,11 @@ return(
 )
 }
 
+function handleButtonClick() {
+    vapi.start("47c7afde-9078-4060-957a-2b542e478733");
+}
+
 export default ChatPage; 
+
+
+
